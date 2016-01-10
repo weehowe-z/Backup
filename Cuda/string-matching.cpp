7fa90859e572d80e3446f0c8baf1c9acfcf7b19d
weehowe-z <weehowe.z@gmail.com>
2016-01-10 11:34:41 +0800

    update

--- Cuda/string-matching.cpp
+++ Cuda/string-matching.cpp
@@ -4,9 +4,10 @@
 #include <time.h>
 using namespace std;
 
+
+void preKMP(string pattern, unsigned int next[]);
+void KMPSearch(string target, string pattern);
 void BFSearch(string target, string pattern);
-
-
 
 int main(int argc, char const *argv[])
 {
@@ -20,7 +21,7 @@ int main(int argc, char const *argv[])
 	in >> target;
 	in >> pattern;
 	BFSearch(target,pattern);
-
+	KMPSearch(target,pattern);
 
 
 
@@ -40,6 +41,7 @@ void BFSearch(string target, string pattern)
 	unsigned int target_len = target.length();
 	unsigned int pattern_len = pattern.length();
 	unsigned int i = 0;
+
 	while (i <= target_len - pattern_len){
 		unsigned int j = 0;
 		while (j < pattern_len){
@@ -58,26 +60,66 @@ void BFSearch(string target, string pattern)
 	cout<<"--------------------\n";
 }
 
-void preKMP(string pattern, int next[])
+void preKMP(string pattern,unsigned int next[])
 {
 	unsigned int pattern_len = pattern.length();
 	unsigned int k; //  longest suffix
     next[0] = 0;
     for (int i = 1; i < pattern_len; ++i)
     {
-        k = next[i - 1];
-        while (k >= 0)
-        {
-            if (pattern[k] == pattern[i])
-                break;
-            else
-                k = next[k];
-        }
-        next[i] = k + 1;
+		while(k > 0 && pattern[i] != pattern[k]) k = next[k-1];
+		if (pattern[i] == pattern[k]) ++k;
+		next[i] = k;	
     }
 }
 
 
 void KMPSearch(string target, string pattern){
+	clock_t begin, end;
+	double time_spent;
+
+	cout<<"--------------------\n";
+	cout<<"Alogrithm:\tKMP\n\n";
+	begin = clock();
+
+	unsigned int target_len = target.length();
+	unsigned int pattern_len = pattern.length();
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int *next = new unsigned int[pattern_len];
+
+	preKMP(pattern,next);
+
+	while (i <= target_len - pattern_len){
+		// cout<<"OUT target["<<i<<"]\n";
+		// cout<<"OUT pattern["<<j<<"]\n";
+		while (j < pattern_len){
+			// cout<<"inner "<<j<<endl;
+			if ((target[i+j]) != pattern[j]) break;
+			++j;
+		}
+		// cout<<"current j "<<j<<endl;
+		if (j == 0){
+			++i;
+		}
+		else if  (j == pattern_len) {
+			cout<<"Match @ position "<< i << endl;
+			j = 0;
+			++i;
+		}
+		else {
+			i += j - next[j-1];
+			j = next[j-1]; 
+			// cout<<"afedfdfd "<<j<<endl;
+		}
+	}
+
+
+	delete []next;
+
+	end = clock();
+	time_spent = (double)(end - begin)* 1000 / CLOCKS_PER_SEC;
+	cout<<"\ntime spent:\t"<<time_spent<<"ms"<<endl;
+	cout<<"--------------------\n";
 
 }
