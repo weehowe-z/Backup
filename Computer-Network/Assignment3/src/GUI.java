d880acc582f4e587b8af34590c1d4b053ef34048
weehowe-z <weehowe.z@gmail.com>
2016-03-30 23:07:08 +0800

    combine gui with socket class

--- Computer-Network/Assignment3/src/GUI.java
+++ Computer-Network/Assignment3/src/GUI.java
@@ -3,25 +3,26 @@ package FileSocket;
 import java.awt.*;
 import java.awt.event.*;
 
-public class AwtListenerDemo {
+public class GUI {
    private Frame mainFrame;
    private Label headerLabel;
    private Label statusLabel;
    private Panel controlPanel;
    private TextField textField;
+   private boolean Flag = true;//whether has started a surver thread
 
-   public AwtListenerDemo(){
+   public GUI(){
       prepareGUI();
    }
 
    public static void main(String[] args){
-      AwtListenerDemo  awtListenerDemo = new AwtListenerDemo();
-      awtListenerDemo.showKeyListenerDemo();
+      GUI demo = new GUI();
+      demo.show();
    }
 
    private void prepareGUI(){
       mainFrame = new Frame("File Share Socket Demo");
-      mainFrame.setSize(400,400);
+      mainFrame.setSize(500,500);
       mainFrame.setLayout(new GridLayout(3, 1));
       mainFrame.addWindowListener(new WindowAdapter() {
          public void windowClosing(WindowEvent windowEvent){
@@ -33,7 +34,7 @@ public class AwtListenerDemo {
       headerLabel.setAlignment(Label.CENTER);
       statusLabel = new Label();
       statusLabel.setAlignment(Label.CENTER);
-      statusLabel.setSize(350,100);
+      statusLabel.setSize(200,100);
 
       controlPanel = new Panel();
       controlPanel.setLayout(new FlowLayout());
@@ -44,10 +45,12 @@ public class AwtListenerDemo {
       mainFrame.setVisible(true);
    }
 
-   private void showKeyListenerDemo(){
+   private void show(){
+      runServer();
       headerLabel.setText("Press <ENTER> to begin download.");
       mainFrame.addKeyListener(new CustomKeyListener());
       textField  = new TextField(10);
+
       // Button button = new Button("Download");
       // button.addActionListener(new ActionListener() {
       //    public void actionPerformed(ActionEvent e) {
@@ -57,6 +60,25 @@ public class AwtListenerDemo {
       // controlPanel.add(textField);
       // controlPanel.add(button);
       mainFrame.setVisible(true);
+      statusLabel.setText("Socket running, you can see the terminal for more details.");
+   }
+
+   private void runServer() {
+      if (Flag) {
+          Flag = false;
+          System.out.println("[GUI]:Server thread begin....");
+          Thread Server = new Thread(new Server());
+          Server.start();
+      }
+      else {
+        System.out.println("[GUI]:Server already started, don't press <ENTER> again!");
+      }
+   }
+
+   private void runClient() {
+        System.out.println("[GUI]:Client thread begin....");
+        Thread Client = new Thread(new Client());
+        Client.start();
    }
 
    class CustomKeyListener implements KeyListener{
@@ -66,11 +88,18 @@ public class AwtListenerDemo {
       public void keyPressed(KeyEvent e) {
          if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
             // statusLabel.setText("Entered text: " + textField.getText());
-            // statusLabel.setText("Begin Download");
             System.exit(-1);
          }
          else if (e.getKeyCode() == KeyEvent.VK_ENTER){
-           statusLabel.setText("Socket running");
+           statusLabel.setText("Download finish, press <ESC> to quit.");
+          //  runServer();
+          //  try {
+          //    Thread.sleep(2000);
+          //  }
+          //  catch (Exception exception){
+          //    System.out.println(exception);
+          //  }
+           runClient();
          }
       }
 
