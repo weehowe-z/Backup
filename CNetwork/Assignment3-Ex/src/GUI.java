9538c5875af1e19e57e84aaf6b5a4216be4d5fc2
weehowe-z <weehowe.z@gmail.com>
2016-04-05 22:39:04 +0800

    update

--- CNetwork/Assignment3-Ex/src/GUI.java
+++ CNetwork/Assignment3-Ex/src/GUI.java
@@ -1,5 +1,6 @@
 package FileSocket;
 
+import java.io.File;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -9,9 +10,12 @@ public class GUI {
    private Label statusLabel;
    private Panel controlPanel;
    private TextField textField;
+   private FileDialog fd;//file selector
    private boolean Flag = true;//whether has started a surver thread
-   public String filePath = null;
-
+   private TextField portText;
+   private TextField addressText;
+   private File file = null;
+   private String filename = null;
 
    public GUI(){
       prepareGUI();
@@ -24,7 +28,7 @@ public class GUI {
 
    private void prepareGUI(){
       mainFrame = new Frame("File Share Socket Demo");
-      mainFrame.setSize(500,500);
+      mainFrame.setSize(400,500);
       mainFrame.setLayout(new GridLayout(3, 1));
       mainFrame.addWindowListener(new WindowAdapter() {
          public void windowClosing(WindowEvent windowEvent){
@@ -48,60 +52,109 @@ public class GUI {
    }
 
    private void show(){
-      runServer();
       headerLabel.setText("Press <ENTER> to begin download.");
-      mainFrame.addKeyListener(new CustomKeyListener());
-      textField  = new TextField(10);
-      FileDialog fd = new FileDialog(mainFrame, "Choose a file", FileDialog.LOAD);
-      Button chooseButton = new Button("Choose Upload File");
-      // Button pathButton = new Button("Choose Download Path");
-      chooseButton.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) {
-            // filePath = chooseFile();
-            fd.setVisible(true);
-            String filename = fd.getFile();
-            String directory = fd.getDirectory();
-            if (filename == null)
-              System.out.println("You cancelled the choice");
-             // return null;
-            else
-              System.out.println("You chose " + filename);
-              System.out.println("Path: " + directory);
-              // return (directory + filename);
+      // mainFrame.addKeyListener(new CustomKeyListener());
 
-            System.out.println(filePath);
+      //As Server
+      fd = new FileDialog(mainFrame, "Select file");
+      Label portLabel = new Label("[Server]Please enter server's port number:");
+      Button chooseButton = new Button("Choose File");
+      Button serverStartButton = new Button("Start Server");
+      portText  = new TextField("2680",6);
+
+      chooseButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+             chooseFile();
          }
       });
-      // Button pathButton = new Button("Choose Download Path");
-      // // button.addActionListener(new ActionListener() {
-      // //    public void actionPerformed(ActionEvent e) {
-      // //       statusLabel.setText("Entered text:");
-      // //    }
-      // // });
-      // // controlPanel.add(textField);
-      //
-      // chooseButton.setOnAction(
-      // new EventHandler<ActionEvent>() {
-      //   @Override
-      //   public void handle(final ActionEvent e) {
-      //       File file = fileChooser.showOpenDialog(stage);
-      //       if (file != null) {
-      //           openFile(file);
-      //       }
-      //   }
-      // });
 
+      serverStartButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+             startServer();
+         }
+      });
+
+      controlPanel.add(portLabel);
+      controlPanel.add(portText);
       controlPanel.add(chooseButton);
+      controlPanel.add(serverStartButton);
+
+      Label splitLabel = new Label("-----------------------------------------------------------------------------------------------------");
+      controlPanel.add(splitLabel);
+
+      //As Client
+      Label addressLabel = new Label("[Client]Please enter the target url:");
+      Button downloadStartButton = new Button("Start Download");
+      addressText  = new TextField("0.0.0.0:2680",18);
+
+      downloadStartButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+             startDownload();
+         }
+      });
+
+      controlPanel.add(addressLabel);
+      controlPanel.add(addressText);
+      controlPanel.add(downloadStartButton);
+
+
+
+
       // controlPanel.add(pathButton);
       mainFrame.setVisible(true);
-      statusLabel.setText("Socket running, you can see the terminal for more details.");
+      // statusLabel.setText("Socket running, you can see the terminal for more details.");
    }
 
-   private void runServer() {
+   private void chooseFile(){
+       fd.setVisible(true);
+       File files[] = fd.getFiles();
+       if (files.length == 1){
+         filename = fd.getFile();
+         file = fd.getFiles()[0];
+         System.out.println("[GUI]:You've selected '" + filename +"'");
+         statusLabel.setText("You've selected '" + filename +"'");
+       }
+   }
+
+   private void startServer(){
+       if (file == null){
+         System.out.println("[GUI]:Please choose a file to send!");
+         statusLabel.setText("Please choose a file to send!");
+         return;
+       }
+       if (portText.getText().equals("")){
+         runServer(file,filename,2680);
+       }
+       else {
+         int portNumber = Integer.parseInt(portText.getText());
+         runServer(file,filename,portNumber);
+       }
+   }
+
+   private void startDownload(){
+       if (addressText.getText().equals("")){
+         System.out.println("[GUI]:Please specify the download url!");
+         statusLabel.setText("Please specify the download url!");
+         return;
+       }
+       String url = addressText.getText();
+       String attrs[] = url.split(":");
+       if (attrs.length != 2){
+         System.out.println("[GUI]:Please specify correct url!");
+         statusLabel.setText("Please specify the correct url!");
+         return;
+       }
+       runClient(attrs[0],Integer.parseInt(attrs[1]));
+   }
+
+   private void runServer(File file, String filename, int portNumber) {
       if (Flag) {
           Flag = false;
           System.out.println("[GUI]:Server thread begin....");
-          Thread Server = new Thread(new Server());
+          Thread Server = new Thread(new Server(file,filename,portNumber));
           Server.start();
       }
       else {
@@ -109,39 +162,35 @@ public class GUI {
       }
    }
 
-   private void runClient() {
+   private void runClient(String hostname, int portNumber) {
         System.out.println("[GUI]:Client thread begin....");
-        Thread Client = new Thread(new Client());
+        Thread Client = new Thread(new Client(hostname, portNumber));
         Client.start();
    }
 
-   private String chooseFile() {
-
-   }
-
-   class CustomKeyListener implements KeyListener{
-      public void keyTyped(KeyEvent e) {
-      }
-
-      public void keyPressed(KeyEvent e) {
-         if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
-            // statusLabel.setText("Entered text: " + textField.getText());
-            System.exit(-1);
-         }
-         else if (e.getKeyCode() == KeyEvent.VK_ENTER){
-           statusLabel.setText("Download finish, press <ESC> to quit.");
-          //  runServer();
-          //  try {
-          //    Thread.sleep(2000);
-          //  }
-          //  catch (Exception exception){
-          //    System.out.println(exception);
-          //  }
-           runClient();
-         }
-      }
-
-      public void keyReleased(KeyEvent e) {
-      }
-   }
+  //  class CustomKeyListener implements KeyListener{
+  //     public void keyTyped(KeyEvent e) {
+  //     }
+   //
+  //     public void keyPressed(KeyEvent e) {
+  //        if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+  //           // statusLabel.setText("Entered text: " + textField.getText());
+  //           System.exit(-1);
+  //        }
+  //        else if (e.getKeyCode() == KeyEvent.VK_ENTER){
+  //          statusLabel.setText("Download finish, press <ESC> to quit.");
+  //         //  runServer();
+  //         //  try {
+  //         //    Thread.sleep(2000);
+  //         //  }
+  //         //  catch (Exception exception){
+  //         //    System.out.println(exception);
+  //         //  }
+  //          runClient();
+  //        }
+  //     }
+   //
+  //     public void keyReleased(KeyEvent e) {
+  //     }
+  //  }
 }
--- CNetwork/Assignment3-Ex/src/Server.java
+++ CNetwork/Assignment3-Ex/src/Server.java
@@ -11,21 +11,20 @@ import java.io.OutputStream;
 
 public class Server implements Runnable {
     // public static final File myFile = new File("helloworld.txt");
-    public void run (){
-      File file = new File("test.zip");
+    public int portNumber;
+    public String filename;
+    public File file;
 
-      int portNumber = 2680;
-      // if (args.length == 0){
-      //     portNumber = 2680;
-      // }
-      // else if (args.length == 1){
-      //     portNumber = Integer.parseInt(args[0]);
-      // }
-      // else {
-      //     System.out.println("Please give correct args or omit it to use default setting!");
-      //     System.exit(-1);
-      //     return;
-      // }
+    public Server(File f,String n, int p) {
+      file = f;
+      filename = n;
+      portNumber = p;
+    }
+
+    public void run (){
+      // File file = new File("test.zip");
+
+      // int portNumber = 2680;
       System.out.println("Server address:\t\thttp://0.0.0.0:" + Integer.toString(portNumber) + "/");
 
       ServerSocket serverSocket = null;
@@ -39,7 +38,7 @@ public class Server implements Runnable {
 
       try {
           while (true) {
-              System.out.println("server is listening!");
+              System.out.println("Server is listening!");
               Socket clientSocket = serverSocket.accept();
               int count;
               byte[] buffer = new byte[16*1024];
@@ -68,56 +67,4 @@ public class Server implements Runnable {
         }
     }
 
-    // 单独使用
-    // public static void main(String[] args) throws Exception {
-    //     System.out.println(args.length);
-    //     File file = new File("test.zip");
-    //
-    //     int portNumber;
-    //     if (args.length == 0){
-    //         portNumber = 2680;
-    //     }
-    //     else if (args.length == 1){
-    //         portNumber = Integer.parseInt(args[0]);
-    //     }
-    //     else {
-    //         System.out.println("Please give correct args or omit it to use default setting!");
-    //         System.exit(-1);
-    //         return;
-    //     }
-    //     System.out.println("Server address:\t\thttp://0.0.0.0:" + Integer.toString(portNumber) + "/");
-    //
-    //     ServerSocket serverSocket = null;
-    //     try {
-    //         serverSocket = new ServerSocket(portNumber);
-    //     } catch (IOException e){
-    //         System.out.println("Error! Can't setup server on this port number, may be occupied.");
-    //         System.exit(-1);
-    //     }
-    //     System.out.println("Server running...\tPress ctrl-c to stop.");
-    //
-    //     try {
-    //         while (true) {
-    //             System.out.println("server is listening!");
-    //             Socket clientSocket = serverSocket.accept();
-    //             int count;
-    //             byte[] buffer = new byte[16*1024];
-    //             InputStream in = new FileInputStream(file);
-    //             OutputStream out = clientSocket.getOutputStream();
-    //             while ((count = in.read(buffer)) > 0) {
-    //                 out.write(buffer, 0, count);
-    //             }
-    //             clientSocket.close();
-    //             out.close();
-    //             in.close();
-    //         }
-    //       } catch (IOException e){
-    //           System.out.println("Error! Socket accept IO Exception.");
-    //           System.exit(-1);
-    //       } finally{
-    //           System.out.println("close listening");
-    //           serverSocket.close();
-    //           System.exit(0);
-    //       }
-    // }
 }
