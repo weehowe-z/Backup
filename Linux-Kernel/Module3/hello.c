b4c9719a91814f7080892e5363dd64c2d5d06802
weehowe-z <weehowe.z@gmail.com>
2016-04-12 20:40:38 +0800

    add module3

--- Linux-Kernel/Module3/hello.c
+++ Linux-Kernel/Module3/hello.c
@@ -1,38 +1,49 @@
-// #include <linux/kernel.h> /* Needed for KERN_INFO */
-// #include <linux/module.h> /* Needed by all modules */
-// #include <linux/init.h> /*Needed for init and cleanup functions*/
-//
-// static int __init hello_init(void)
-// {
-//   printk(KERN_INFO "hello ZWH!\n");
-//   return 0;
-// }
-//
-// static void __exit hello_exit(void)
-// {
-//   printk(KERN_INFO "Goodbye!\n");
-// }
-// moudle_init(hello_init);
-// moudle_exit(hello_exit);
-
-
-
-/*
- *  hello-1.c - The simplest kernel module.
- */
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 
-int init_module(void)
-{
-	printk(KERN_INFO "Hello ZWH\n");
-	/*
-	* A non 0 return means init_module failed; module can't be loaded.
-	*/
-	return 0;
+#define proc_name  "myproc"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Wenhao Zhu");
+
+struct proc_dir_entry *entry;
+
+static int hello_proc_show(struct seq_file *m, void *v) {
+  seq_printf(m, "Hello this is a new proc!\n");
+  return 0;
 }
 
-void cleanup_module(void)
+static int hello_proc_open(struct inode *inode, struct  file *file) {
+  return single_open(file, hello_proc_show, NULL);
+}
+
+static const struct file_operations hello_proc_fops = {
+  .owner = THIS_MODULE,
+  .open = hello_proc_open,
+  .read = seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,
+};
+
+int init_module()
 {
-	printk(KERN_INFO "Goodbye world 1.\n");
+	entry = proc_create(proc_name, 0, NULL, &hello_proc_fops);
+
+	if (!entry) {
+		remove_proc_entry(proc_name, NULL);
+		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",proc_name);
+		return -ENOMEM;
+	}
+
+
+	printk(KERN_INFO "/proc/%s created\n", proc_name);
+	return 0;	/* everything is ok */
+}
+
+void cleanup_module()
+{
+	remove_proc_entry(proc_name,NULL);
+	printk(KERN_INFO "/proc/%s removed\n", proc_name);
 }
