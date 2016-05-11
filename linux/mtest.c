ad0759bb97e3e6ed6897b454ecbc55dade199b0d
weehowe-z <weehowe.z@gmail.com>
2016-05-11 18:51:11 +0800

    add agrs & error check

--- linux/mtest.c
+++ linux/mtest.c
@@ -1,27 +1,38 @@
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/module.h>/* Needed by all modules */
+#include <linux/kernel.h>/* Needed for KERN_INFO */
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>/* Needed for seq interface */
 #include <linux/sched.h>/* Needed for task_struct*/
-#include <linux/mm.h> /* Needed for vm_flags */
+#include <linux/mm.h>/* Needed for vm_flags */
 #include <asm/uaccess.h>/* Needed for copy_from_user */
 
 #define proc_name  "mtest"
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Memory management");
+MODULE_DESCRIPTION("Memory Management Homework");
 MODULE_AUTHOR("weehowe.z@gmail.com");
 
 char buf[128];/* proc file content */
 
 struct proc_dir_entry *entry;
 
-static void listvma(void){
-	struct task_struct *task = current;/* get the task_struct of the current process */
-	/* or get the task_struct by pid */
-	/* struct task_struct *task = pid_task(find_get_pid(pid),PIDTYPE_PID); */
-	struct mm_struct *mm = task->mm;
-	struct vm_area_struct *vma = mm->mmap ;/*this struct defines a memory VMM memory area*/
+static void listvma(unsigned int pid){
+
+	struct task_struct *task;
+	struct mm_struct *mm;
+	struct vm_area_struct *vma; /*this struct defines a memory VMM memory area*/
+
+	if (pid == 0){
+		task = current; /* get the task_struct of the current process */
+	}	else {
+		task = pid_task(find_get_pid(pid),PIDTYPE_PID); /* or get the task_struct by pid */
+		if (!task){
+			printk(KERN_ALERT "Invalid pid %d, no such process\n", pid);
+			return;
+		}
+	}
+	mm = task->mm;
+	vma = mm->mmap ;
 
 	printk(KERN_INFO "start-addr\tend-addr\tpermission\n");
 	while (vma){
@@ -41,34 +52,53 @@ static void listvma(void){
 	}
 }
 
+static void findpage(void){
+
+}
+
 static int proc_show(struct seq_file *m, void *v) {
 	printk(KERN_INFO "/proc/%s read called\n", proc_name);
 	seq_printf(m, "%s", buf);
-  return 0;
+	return 0;
 }
 
 static int proc_open(struct inode *inode, struct  file *file) {
-  return single_open(file, proc_show, NULL);
+	return single_open(file, proc_show, NULL);
 }
 
 static ssize_t proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *data){
+	unsigned int pid;
+	unsigned long addr, val;
 	printk(KERN_INFO "/proc/%s write called\n", proc_name);
 	memset(buf,0,sizeof(buf)/sizeof(char));//clear the buffer
 
 	if (count > sizeof(buf)) return -EINVAL;
 	if (copy_from_user(buf, buffer, count)) return -EFAULT;
 
-	if (memcmp(buf,"listvma",7) == 0 && count == 8) {
+	if (memcmp(buf,"listvma",7) == 0) {
 		printk(KERN_INFO "/proc/%s listvma called\n", proc_name);
-		listvma();
+		if (count == 8) listvma(0);/*no pid args*/
+		else if (sscanf(buf+7, "%d", &pid) == 1){
+			printk(KERN_INFO "%d\n", pid);
+			listvma(pid);
+		}
+		else return -EFAULT;
 	}
 	else if (memcmp(buf,"findpage",8) == 0){
 		printk(KERN_INFO "/proc/%s findpage called\n", proc_name);
-
+		if (sscanf(buf+8, "%lx", &addr) == 1){
+			printk(KERN_INFO "%lx\n", addr);
+		}
+		else return -EFAULT;
 	}
 	else if (memcmp(buf,"writeval",8) == 0){
 		printk(KERN_INFO "/proc/%s writeval called\n", proc_name);
+		if (sscanf(buf+8, "%lx %lx", &addr, &val) == 2){
+			printk(KERN_INFO "%lx %lx\n", addr, val);
+		}
+		else return -EFAULT;
 	}
+	
 	return count;
 }
 
--- linux/test.sh
+++ linux/test.sh
@@ -2,11 +2,13 @@
 make
 insmod mtest.ko
 cat /proc/mtest
-echo "hello" > /proc/mtest
-cat /proc/mtest
 echo "listvma" > /proc/mtest
 cat /proc/mtest
-echo "listvma1" > /proc/mtest
+echo "listvma 9181" > /proc/mtest
+cat /proc/mtest
+echo "findpage 0x7fffbda9d000" > /proc/mtest
+cat /proc/mtest
+echo "writeval 0x7fffbda9d000 0x7fffbda9d023" > /proc/mtest
 cat /proc/mtest
 rmmod mtest
 dmesg | tail -50
