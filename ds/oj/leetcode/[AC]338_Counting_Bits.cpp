72001de3e43250ee8b12b4ee698ac843770cf302
weehowe-z <weehowe.z@gmail.com>
2016-09-12 23:35:04 +0800

    add LC338_Counting_Bits

--- ds/oj/leetcode/[AC]338_Counting_Bits.cpp
+++ ds/oj/leetcode/[AC]338_Counting_Bits.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+
 using namespace std;
 
 class Solution {
@@ -7,13 +9,24 @@ public:
     vector<int> countBits(int num) {
         vector<int> vec;
         if (num == 0) {vec.push_back(0);return vec;}
-        if (num == 1) {vec.push_back(1);return vec;}
+        if (num == 1) {vec.push_back(0);vec.push_back(1);return vec;}
 
-        for(int i=0; i<num ;++i){
+        int len = 2;
 
+        vec.push_back(0);
+        vec.push_back(1);
+
+        int level = ceil(sqrt(num)) + 1;
+        cout<<level<<endl;
+        for(int i=1; i<level;++i){
+            for (int j=0; j<pow(2,i); ++j){
+                vec.push_back(vec[j]+1);
+                len++;
+                if (len==num+1) return vec;
+            }
         }
 
-
+        return vec;
     }
 };
 
@@ -21,5 +34,9 @@ int main()
 {
     Solution s;
     cout<<"Hello world!"<<endl;
-    vector<int> vec = s.countBits(5);
+    vector<int> vec = s.countBits(6);
+    for (int i = 0 ; i < vec.size(); ++i){
+        cout<<vec[i]<<" ";
+    }
+    return 0;
 }
