628c703268e1040e0943820e1ea993bf801ca99b
weehowe-z <weehowe.z@gmail.com>
2016-09-12 23:51:46 +0800

    add LC_292_Nim_Game

---
+++ ds/oj/leetcode/[AC]292_Nim_Game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if ( n%4 == 0) return false;
+        else return true;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout<<s.canWinNim(5)<<endl;
+}
